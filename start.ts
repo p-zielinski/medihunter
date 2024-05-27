205fb6e74604c17a5bcee1ec28c4b2a5e435d6ca
p-zielinski <piotrzielinski96@yahoo.com>
2024-05-27 17:52:31 +0200

    fix

--- start.ts
+++ start.ts
@@ -16,14 +16,14 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-const sendEmail = async() =>  {
+const sendEmail = async(text) =>  {
     // send mail with defined transport object
     const info = await transporter.sendMail({
         from: `"${emailAddress}" <${emailAddress}>`, // sender address
         to: emailAddress, // list of receivers
-        subject: "Appointment available", // Subject line
-        text: "Appointment available", // plain text body
-        html: "<b>Appointment available</b>", // html body
+        subject: `Appointment available\n${text}`, // Subject line
+        text: `Appointment available\n${text}`, // plain text body
+        html: `<b>Appointment available\n${text}</b>`.replaceAll('\n','<br />'), // html body
     });
 
     console.log("Message sent: %s", info.messageId);
@@ -34,23 +34,27 @@ const sleep = ms => new Promise(r => setTimeout(r, ms));
 const checkAppointment = async () => await new Promise((resolve) => {
     return exec(process.env.MEDIHUNTER_RUN_COMMAND, (error, stdout, stderr) => {
         if(stdout.includes('(iteration: 1)') && !stdout.includes('No results found')){
-            console.log(stdout)
-            return resolve(true);
+            return resolve(stdout);
         }
         if (stderr) {
             console.log(stderr);
         }
-        return resolve(false);
+        return resolve(undefined);
     });
 });
 
 
 (async ()=>{
-    while(!await checkAppointment()){
+    let appointments;
+    while(true){
+        appointments = await checkAppointment();
+        if(appointments){
+            break;
+        }
         console.log('no appointments available')
         await sleep(1000 * 90)
     }
-    await sendEmail().catch(console.error);
+    await sendEmail(appointments).catch(console.error);
     console.log('appointment found, app closed');
     await sleep(1000 * 60 * 60)
 })()
